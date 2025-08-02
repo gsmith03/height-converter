069a28857f01866dfe31a894a27c32beb1369fed
gsmith03 <gsmithhill@gmail.com>
2025-08-02 18:48:06 -0400

    Changed loader size

--- src/main/java/com/gwenhill/converter/heightconverter/HeightConverterApplication.java
+++ src/main/java/com/gwenhill/converter/heightconverter/HeightConverterApplication.java
@@ -11,7 +11,7 @@ public class HeightConverterApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HeightConverterApplication.class.getResource("height-converter.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        Scene scene = new Scene(fxmlLoader.load(), 450, 400);
         stage.setTitle("Height Converter" + "");
         stage.setScene(scene);
         stage.show();
