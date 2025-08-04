442d38036909aa1e77dd927a520010ded373c6ba
gsmith03 <gsmithhill@gmail.com>
2025-08-04 09:23:02 -0400

    Changed loader size

--- src/main/java/com/gwenhill/converter/heightconverter/HeightConverterApplication.java
+++ src/main/java/com/gwenhill/converter/heightconverter/HeightConverterApplication.java
@@ -3,6 +3,7 @@ package com.gwenhill.converter.heightconverter;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -11,7 +12,8 @@ public class HeightConverterApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HeightConverterApplication.class.getResource("height-converter.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 450, 400);
+        Scene scene = new Scene(fxmlLoader.load(), 550, 450);
+        scene.setFill(Color.BLACK);
         stage.setTitle("Height Converter" + "");
         stage.setScene(scene);
         stage.show();
--- src/main/java/com/gwenhill/converter/heightconverter/HeightConverterController.java
+++ src/main/java/com/gwenhill/converter/heightconverter/HeightConverterController.java
@@ -5,14 +5,29 @@ import javafx.scene.control.Alert;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.control.Button;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.shape.*;
 
 public class HeightConverterController {
     @FXML private TextField feetField;
     @FXML private TextField inchesField;
     @FXML private TextField cmField;
+    @FXML private TextField inchesOutput;
+    @FXML private AnchorPane rootPane;
 
     @FXML private Button clearButton;
     @FXML private Button exitButton;
+
+    @FXML
+    public void initialize() {
+        // Clip the AnchorPane to a rounded rectangle
+        Rectangle clip = new Rectangle();
+        clip.widthProperty().bind(rootPane.widthProperty());
+        clip.heightProperty().bind(rootPane.heightProperty());
+        clip.setArcWidth(40);
+        clip.setArcHeight(40);
+        rootPane.setClip(clip);
+    }
 
     @FXML
     private void handleClear() {
@@ -29,19 +44,22 @@ public class HeightConverterController {
     @FXML
     private void handleConvert() {
         try {
-            double feet, inches, centimeters; // declare variables
+            double feet, inches, centimeters, totalInches; // declare variables
 
             // Read and Convert the User's Input
             feet = Double.parseDouble(feetField.getText());
             inches = Double.parseDouble(inchesField.getText());
 
             // Perform the conversion
-            centimeters = ((feet * 12) + inches) * 2.54;
+            totalInches= (feet * 12) + inches;
+            centimeters = totalInches * 2.54;
+
             //Display the results
+            inchesOutput.setText(String.format("%.2f", totalInches));
             cmField.setText(String.format("%.2f", centimeters));
         }
         catch(NumberFormatException e) {
-            // normal catch that shows error in the cmTextField
+                // normal catch that shows error in the cmTextField
             // cmField.setText("Please enter valid numbers for feet and inches.");
 
             // Pop Up Box Alert
